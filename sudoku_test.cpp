eebe6e2cf19847af7b62f274bb80db91789de3db
nehcizniy <zichen_yin@yahoo.com>
2022-05-12 13:28:32 -0400

    finished checking

--- sudoku_test.cpp
+++ sudoku_test.cpp
@@ -34,7 +34,7 @@ int main(){
     for(int i = 0; i < 9; i ++){
         for(int j = 0; j < 9; j ++){
             if(grid[i][j]){
-                sumofdifference += target[i][j] - grid[i][j];
+                sumofdifference += (target[i][j] != grid[i][j]);
             }
         }
     }
